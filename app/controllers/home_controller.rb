0dc3e2445adb757d1a71d2bd88647da03cc21c63
Harris Novick <harris@harrisnovick.com>
2012-01-17 20:58:37 -0500

    Session button styles

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    @popular_today = Kaminari.paginate_array(Video.from_last(1.day).top(50)).page(params[:page]).per(10)
+    @popular_today = Kaminari.paginate_array(Video.from_last(1.day).top(100)).page(params[:page]).per(10)
   end
 end
